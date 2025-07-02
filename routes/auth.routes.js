f7d750f1253411c930172642e822379c2bb8f20d
Shakti Priya <priyashakti68@gmail.com>
2025-07-02 14:30:49 +0530

    feat: implement category management with CRUD operations; add slugify for category names; update server and routes for category integration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,4 @@
 import express from "express";
-// import {
-//     registerController,
-//     loginController,
-//     testController,
-//     forgotPasswordController,
-//     updateProfileController,
-//     getOrdersController,
-//     getAllOrdersController,
-//     orderStatusController,
-// } from "../controllers/auth.controller.js";
-// import { isAdmin, requireSignIn } from "../middlewares/auth.middleware.js";
 import { loginController, registerController, testController } from '../controllers/auth.controller.js';
 import { isAdmin, requireSignIn } from "../middlewares/auth.middleware.js";
 
@@ -24,12 +13,7 @@ router.post("/register", registerController);
 //LOGIN || POST
 router.post("/login", loginController);
 
-// //Forgot Password || POST
-// router.post("/forgot-password", forgotPasswordController);
 
-// //test routes
-// router.get("/test", requireSignIn, isAdmin, testController);
-//test routes
 router.get("/test", requireSignIn, isAdmin, testController);
 
 //protected User route auth
@@ -38,30 +22,5 @@ router.get("/dash", requireSignIn, (req, res) => {
     // here we wrote the controller directly as it was of just 1 line
     // agar response ok:true rha then only we will be able to access the private route
 });
-
-
-
-
-// //protected Admin route auth
-// router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
-//     res.status(200).send({ ok: true });
-// });
-
-// //update profile
-// router.put("/profile", requireSignIn, updateProfileController);
-
-// //orders
-// router.get("/orders", requireSignIn, getOrdersController);
-
-// //all orders
-// router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
-
-// // order status update
-// router.put(
-//     "/order-status/:orderId",
-//     requireSignIn,
-//     isAdmin,
-//     orderStatusController
-// );
 
 export default router;
--- server.js
+++ server.js
@@ -57,6 +57,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from './routes/auth.routes.js';
 import cors from 'cors';
+import categoryRoutes from './routes/category.routes.js';
 
 dotenv.config();
 connectDB();
@@ -71,7 +72,10 @@ app.use(cors({
 app.use(express.json());
 app.use(morgan('dev'));
 
+
+// routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes); // Category routes
 
 app.get('/', (req, res) => {
     res.send("<h1>Welcome to ecommer app</h1>");
