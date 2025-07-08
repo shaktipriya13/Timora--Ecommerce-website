2b260d57186f4ea3576787ecbfa25149acdfd33e
Shakti Priya <priyashakti68@gmail.com>
2025-07-09 01:41:33 +0530

    update files

--- client/src/pages/Admin/Products.jsx
+++ client/src/pages/Admin/Products.jsx
@@ -159,7 +159,7 @@ const Products = () => {
                                         Update Product
                                     </button>
                                 </div>
- */}
+
                             </div>
 
                             // </Link>
